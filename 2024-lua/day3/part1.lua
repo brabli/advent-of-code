4881b90b926d739af606e1695458a60d85ea1b8d
brabli <67018167+brabli@users.noreply.github.com>
2024-12-14 19:53:28 +0000

    Complete part 1

--- 2024-lua/day3/part1.lua
+++ 2024-lua/day3/part1.lua
@@ -1,8 +1,35 @@
-local input = "./example-input.txt"
+local input = "./input.txt"
+
+local function get_total(line)
+   local running_total = 0
+
+   for match in string.gmatch(line, "mul%(%d+,%d+%)") do
+      local digits = string.gmatch(match, "%d+")
+
+      local x
+      local y
+      for digit in digits do
+         if x == nil then
+            x = digit
+         elseif y == nil then
+            y = digit
+         end
+      end
+
+      running_total = running_total + tonumber(x) * tonumber(y)
+   end
+
+   return running_total
+end
 
 local function main()
-   -- code here
+   local running_total = 0
+
+   for line in io.lines(input) do
+      running_total = running_total + get_total(line)
+   end
+
+   print(running_total)
 end
 
 main()
-
