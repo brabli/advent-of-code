ba78fbd146dccb2448e2968f923d9a66c73b9905
brabli <67018167+brabli@users.noreply.github.com>
2024-12-14 23:13:42 +0000

    Refactor day 3 part 2

--- 2024-lua/day3/part2.lua
+++ 2024-lua/day3/part2.lua
@@ -1,11 +1,5 @@
 local file = "./input.txt"
 
-local function print_table(table)
-   for _, value in pairs(table) do
-      print(value)
-   end
-end
-
 local function multiply_mul(mul)
    local digits = string.gmatch(mul, "%d+")
 
@@ -22,63 +16,68 @@ local function multiply_mul(mul)
    return tonumber(x) * tonumber(y)
 end
 
-local function get_total(line)
-   local mul_pattern = "mul%(%d+,%d+%)"
-   local do_pattern = "do%(%)"
-   local dont_pattern = "don't%(%)"
-   local matches = {}
-
+local function find_matches(input, pattern, matches)
    local current_index = 1
-   for match in string.gmatch(line, mul_pattern) do
-      local start_of_match_index = string.find(line, match, current_index, true) or 0
+   for match in string.gmatch(input, pattern) do
+      local _, end_of_match_index = string.find(input, match, current_index, true)
 
-      matches[start_of_match_index] = match
-      current_index = start_of_match_index + 1
+      if end_of_match_index ~= nil then
+         matches[end_of_match_index] = match
+         current_index = end_of_match_index
+      end
    end
+end
 
-   current_index = 1
-   for match in string.gmatch(line, do_pattern) do
-      local start_of_match_index = string.find(line, match, current_index, true) or 0
-
-      matches[start_of_match_index] = match
-      current_index = start_of_match_index + 1
-   end
-
-   current_index = 1
-
-   for match in string.gmatch(line, dont_pattern) do
-      local start_of_match_index = string.find(line, match, current_index, true) or 0
-
-      matches[start_of_match_index] = match
-      current_index = start_of_match_index + 1
-   end
-
+local function order_by_keys(tbl)
    local tkeys = {}
-   for key in pairs(matches) do
+   for key in pairs(tbl) do
       table.insert(tkeys, key)
    end
+
    table.sort(tkeys)
 
-   local ordered_matches = {}
+   local ordered_tbl = {}
    for _, value in pairs(tkeys) do
-      table.insert(ordered_matches, matches[value])
+      table.insert(ordered_tbl, tbl[value])
    end
 
-   local total = 0
-   local enabled = true
-   for _, instruction in pairs(ordered_matches) do
-      if string.find(instruction, mul_pattern) then
+   return ordered_tbl
+end
+
+local function calculate_answer(instructions)
+   local total, enabled = 0, true
+
+   for _, instruction in pairs(instructions) do
+      if string.find(instruction, Mul_pattern) then
          if enabled then
             total = total + multiply_mul(instruction)
          end
-      elseif string.find(instruction, do_pattern) then
+      elseif string.find(instruction, Do_pattern) then
          enabled = true
-      elseif string.find(instruction, dont_pattern) then
+      elseif string.find(instruction, Dont_pattern) then
          enabled = false
       else
          print("Invalid: " .. instruction)
       end
    end
+
+   return total
+end
+
+local function get_total(line)
+   Mul_pattern = "mul%(%d+,%d+%)"
+   Do_pattern = "do%(%)"
+   Dont_pattern = "don't%(%)"
+
+   local matches = {}
+
+   find_matches(line, Mul_pattern, matches)
+   find_matches(line, Do_pattern, matches)
+   find_matches(line, Dont_pattern, matches)
+
+   local ordered_matches = order_by_keys(matches)
+
+   local total = calculate_answer(ordered_matches)
 
    return total
 end
