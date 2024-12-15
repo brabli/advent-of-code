3f96a25ed9194b99af2d5ec1d6d6379f9ca6657c
brabli <67018167+brabli@users.noreply.github.com>
2024-12-15 17:39:26 +0000

    Work on day 4

--- 2024-lua/day4/part1.lua
+++ 2024-lua/day4/part1.lua
@@ -1,8 +1,44 @@
-local input = "./example-input.txt"
+local input_path = "./example-input.txt"
+
+local function print_grid(grid)
+   print()
+
+   for _, value in pairs(grid) do
+      if "table" == type(value) then
+         print(table.concat(value))
+      end
+   end
+
+   print()
+   print("Height: " .. grid.height)
+   print("Width: " .. grid.width)
+   print()
+end
+
+local function create_grid()
+   local grid = {}
+
+   local r = 1
+   for row in io.lines(input_path) do
+      grid[r] = {}
+
+      for i = 1, #row do
+         local char = string.sub(row, i, i)
+         grid[r][i] = char
+      end
+
+      r = r + 1
+   end
+
+   grid.width = #grid[1]
+   grid.height = r
+
+   return grid
+end
 
 local function main()
-   -- code here
+   local grid = create_grid()
+   print_grid(grid)
 end
 
 main()
-
