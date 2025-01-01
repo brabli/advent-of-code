9b3a37de3451b9b2ad261ccda3662c088984184d
brabli <67018167+brabli@users.noreply.github.com>
2025-01-01 22:14:44 +0000

    Start day 6

--- 2024-lua/day6/part1.lua
+++ 2024-lua/day6/part1.lua
@@ -1,8 +1,43 @@
 local input = "./example-input.txt"
 
-local function main()
-   -- code here
+local function get_start_coords()
+   local row_index = 0
+
+   for line in io.lines(input) do
+      row_index = row_index + 1
+
+      local col_index = 0
+
+      for c in string.gmatch(line, ".") do
+         col_index = col_index + 1
+
+         if c == "^" then
+            return { col_index, row_index }
+         end
+      end
+   end
 end
 
-main()
+local function get_obstacles()
+   local obstacles = {}
+   local row_index = 0
 
+   for line in io.lines(input) do
+      row_index = row_index + 1
+
+      local col_index = 0
+
+      for c in string.gmatch(line, ".") do
+         col_index = col_index + 1
+
+         if c == "#" then
+            table.insert(obstacles, { col_index, row_index })
+         end
+      end
+   end
+
+   return obstacles
+end
+
+local starting_coords = get_start_coords()
+local obstacles = get_obstacles()
