4286bbe55276c9d04aba7b65446ea6c6bd2e2ccc
brabli <67018167+brabli@users.noreply.github.com>
2025-12-31 18:43:56 +0000

    Work on part 2

--- 2025-rust/day2/src/main.rs
+++ 2025-rust/day2/src/main.rs
@@ -4,7 +4,7 @@ use std::{fs, ops::RangeInclusive};
 
 fn main() {
     let ranges = {
-        let content_string = fs::read_to_string("input.txt").unwrap();
+        let content_string = fs::read_to_string("example.txt").unwrap();
         let split_strings = content_string.trim().split(",");
 
         split_strings
@@ -12,31 +12,38 @@ fn main() {
             .collect::<Vec<Range>>()
     };
 
-    let mut invalid_ids = vec![];
+    let invalid_ids = vec![];
 
     for range in ranges {
+        // Looping over the numbers in a single Range
         for i in range.into_iter() {
-            let string_number = i.to_string();
-            let len = string_number.len();
-            if len % 2 != 0 {
-                continue;
+            let divisors_per_number = {
+                let len = i.to_string().len();
+                let mut divisors = vec![];
+
+                for i in 2..=len {
+                    if len % i == 0 {
+                        divisors.push(i);
+                    }
+                }
+
+                divisors
+            };
+
+            for divisor in divisors_per_number {
+                let num_as_chars = i.to_string().chars().collect::<Vec<char>>();
+                let vec_of_chunks = num_as_chars.chunks_exact(divisor).collect::<Vec<&[char]>>();
+                for chunk_of_chars in vec_of_chunks {
+                    let num_as_string = chunk_of_chars.iter().collect::<String>();
+                    let num = num_as_string.parse::<i64>().unwrap();
+                }
             }
-
-            let middle_index = len / 2;
-
-            let (first, second) = string_number.split_at(middle_index);
-
-            if first != second {
-                continue;
-            }
-
-            invalid_ids.push(i);
         }
     }
 
     let answer = invalid_ids.iter().fold(0, |acc, i| acc + i);
 
-    dbg!(answer);
+    // dbg!(answer);
 }
 
 #[derive(Debug)]
